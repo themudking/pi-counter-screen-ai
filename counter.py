1203695dfee1e50c0e2e52c32a9de0ba400b9daf
Curtis Thornton <themudking@gmail.com>
2025-06-14 21:50:17 -0400

    Update background color of day display label for improved visibility

--- counter.py
+++ counter.py
@@ -44,7 +44,7 @@ class StopwatchApp:
             time_display_frame,
             font=self.days_font,
             fg='white',
-            bg='#333333'
+            bg="#042A7A"
         )
         # Note: The label is not packed here. It will be packed in update_time().
 
